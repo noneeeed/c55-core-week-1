b8b65c11d3627f66b1b3ec85ef8bdb4c6ffcc3af
Mohamad Bader almsaddi alzin <baderalmsaddy@gmail.com>
2026-01-14 11:10:36 +0100

    task 1, task 2 added

--- task-1/setup.sh
+++ task-1/setup.sh
@@ -1,1 +1,34 @@
-# Write your code here
+echo "Creating project…"
+mkdir project;
+cd project
+git init
+mkdir resources src
+touch settings.conf
+cd resources
+touch "family picture.jpg" icon.png logo.png
+cd ../src
+mkdir database profile
+touch program.java
+git add -A
+git commit -m "initial commit"
+sleep 3
+echo "Setup projet..."
+echo "Welcome to my project" >README.md
+cd src
+rmdir profile
+cd ../resources
+rm "family picture.jpg"
+git add -A
+git commit -m "second commit" -m "removed family picture Woopsie😁 and my personal profile"
+sleep 3
+ls
+echo "Setup javascript…"
+cd ../src
+git mv program.java program.js
+echo 'console.log("JavaScript Works!");' >program.js
+node program.js
+git add -A
+git commit -m "third commit" -m "java script works now/ renamed program and added 1 line of code"
+cd ~
+ls
+echo 'All done!'
